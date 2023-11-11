19a19c6b61ec177424c7ddb766c9dbf00ef82036
mywhzy <cmj2393a@naver.com>
2023-11-11 23:53:19 +0900

    feat: useSWR사용해 가입 유저 리스트 페이지 구현

--- src/pages/UserListPage.tsx
+++ src/pages/UserListPage.tsx
@@ -1,3 +1,24 @@
+import useSWR from 'swr';
+import { BASE_URL } from '../constants';
+import { User } from '../types';
+
+// TODO: fetcher 전역으로 빼기
+const fetcher = (url: string) =>
+  fetch(BASE_URL + url).then((res) => res.json());
+
 export const UserListPage = () => {
-  return <>유저 목록 페이지</>;
+  const { data, error } = useSWR('/api/users', fetcher);
+  const users: User[] = data?.data.users;
+
+  return (
+    <>
+      <p>가입 유저 목록</p>
+      {users &&
+        users.map((user) => (
+          <div key={user.email}>
+            username: {user.username} &nbsp; email: {user.email}
+          </div>
+        ))}
+    </>
+  );
 };
