c4a38d7bbd57ccec19231aa16998c0e49f21f404
mywhzy <cmj2393a@naver.com>
2023-11-13 19:46:12 +0900

    feat: fetcher 전역 설정 위해 SWRConfig 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
+import { SWRConfig } from 'swr';
+import { BASE_URL } from './constants';
 import { LoginPage } from './pages/LoginPage';
 import { RegisterPage } from './pages/RegisterPage';
 import { UserListPage } from './pages/UserListPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="register" element={<RegisterPage />} />
-      <Route path="/users" element={<UserListPage />} />
-    </Routes>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) =>
+          fetch(BASE_URL + url).then((res) => res.json()),
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="/users" element={<UserListPage />} />
+      </Routes>
+    </SWRConfig>
   );
 }
 
--- src/pages/UserListPage.tsx
+++ src/pages/UserListPage.tsx
@@ -1,22 +1,18 @@
 import useSWR from 'swr';
-import { BASE_URL } from '../constants';
 import { User } from '../types';
 
-// TODO: fetcher 전역으로 빼기
-const fetcher = (url: string) =>
-  fetch(BASE_URL + url).then((res) => res.json());
-
 export const UserListPage = () => {
-  const { data, error } = useSWR('/api/users', fetcher);
+  const { data } = useSWR('/api/users');
+
   const users: User[] = data?.data.users;
 
   return (
     <>
-      <p>가입 유저 목록</p>
+      <h3>가입 유저 목록</h3>
       {users &&
         users.map((user) => (
           <div key={user.email}>
-            username: {user.username} &nbsp; email: {user.email}
+            <b>username: </b> {user.username} <b>email: </b> {user.email}
           </div>
         ))}
     </>
