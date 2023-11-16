ae1b24f16470b81a820363aeff1ad1eb23f5f67a
mywhzy <cmj2393a@naver.com>
2023-11-16 16:18:10 +0900

    feat: 회원가입 페이지 formik 적용

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,3 +1,48 @@
+import { useFormik } from 'formik';
+
 export const RegisterPage = () => {
-  return <> 회원가입 페이지 </>;
+  const formik = useFormik({
+    initialValues: {
+      userName: '',
+      email: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      // api 연결 전 테스팅용 콘솔 출력
+      console.log(values);
+    },
+  });
+
+  return (
+    <>
+      <p>회원가입</p>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text"
+          name="userName"
+          placeholder="User Name"
+          onChange={formik.handleChange}
+          value={formik.values.userName}
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          required
+        />
+        <button type="submit">회원가입</button>
+      </form>
+    </>
+  );
 };
