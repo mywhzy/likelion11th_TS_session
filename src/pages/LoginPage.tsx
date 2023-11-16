ae38143508bd316bbcef5fc4ca43c569b439faa4
mywhzy <cmj2393a@naver.com>
2023-11-17 02:56:32 +0900

    feat: 로그인 기능 구현

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,51 @@
+import { useNavigate } from 'react-router-dom';
+import useSWRMutation from 'swr/mutation';
+import { useFormik } from 'formik';
+import { sendRequest } from '../utils/sendRequest';
+
 export const LoginPage = () => {
-  return <> 로그인 페이지 </>;
+  const navigate = useNavigate();
+
+  const { trigger } = useSWRMutation('api/auth/login', sendRequest, {
+    onSuccess: () => {
+      alert('로그인 성공!');
+      navigate('/');
+    },
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: async (values, { resetForm }) => {
+      await trigger(values);
+      resetForm();
+    },
+  });
+
+  return (
+    <>
+      <p>로그인</p>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          required
+        />
+        <button type="submit">로그인</button>
+      </form>
+    </>
+  );
 };
