e9d8a6287b6434b6e2e41f0d58d4f8ebe580acea
mywhzy <cmj2393a@naver.com>
2023-11-18 02:35:23 +0900

    feat: UserList 컴포넌트 구현

---
+++ src/components/UserList.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+interface UserListProps {
+  username: string;
+  email: string;
+}
+
+export const UserList = ({ username, email }: UserListProps) => {
+  return (
+    <Container>
+      <Body>{email}</Body>
+      <Title>{username}</Title>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 1200px;
+  padding: 14px 16px;
+  border-radius: 12px;
+  box-shadow: 0px 0px 8px 0px #e5e8f0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 12px;
+`;
+
+const Body = styled.p`
+  ${({ theme }) => theme.typography.body1}
+  color: ${({ theme }) => theme.color.gray2};
+`;
+
+const Title = styled.p`
+  ${({ theme }) => theme.typography.title1}
+  color: ${({ theme }) => theme.color.gray1};
+`;
