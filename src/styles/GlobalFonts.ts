0ca9862a48d4e4fd28556d9d786aff4075588dd8
mywhzy <cmj2393a@naver.com>
2023-11-19 01:12:34 +0900

    chore: Pretendard url Bold로 변경

--- src/styles/GlobalFonts.ts
+++ src/styles/GlobalFonts.ts
@@ -11,7 +11,7 @@ export const GlobalFonts = createGlobalStyle`
 
 @font-face {
     font-family: 'Pretendard';
-    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff') format('woff');
     font-weight: 700;
     font-style: normal;
 }
