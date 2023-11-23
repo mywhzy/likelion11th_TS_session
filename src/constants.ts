95e145c64c548049fa32de40ca392797af02f1a1
mywhzy <cmj2393a@naver.com>
2023-11-23 17:50:11 +0900

    chore: 인풋 에러 메시지, 이메일 정규식 상수 추가

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,11 @@
 export const BASE_URL: string = process.env.REACT_APP_API_URL ?? '';
+
+export const INPUT_ERROR_MESSAGE = {
+  requiredUsername: '이름을 입력해주세요',
+  requiredEmail: '이메일을 입력해주세요',
+  requiredPassword: '비밀번호를 입력해주세요',
+  formatEmail: '이메일 형식이 올바르지 않습니다.',
+  duplicateEmail: '이미 존재하는 이메일입니다',
+};
+
+export const EMAIL_REGEX = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
